d549f3bfffa2f0a4050e9b7db857b223d5c3ebe2
keith bernard <kbernard@logikos.com>
2016-12-08 15:08:05 -0500

    Added Windows Service Implementation

---
+++ InsulinWindowsService/InsulinWindowsService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ServiceModel.Web;
+using System.ServiceProcess;
+
+namespace InsulinWindowsService
+{
+
+    // https://msdn.microsoft.com/en-us/library/ms733069.aspx#Y382
+
+    public class InsulinWindowsService : ServiceBase
+    {
+        //public ServiceHost serviceHost = null;
+        public WebServiceHost serviceHost = null;
+
+
+        public InsulinWindowsService()
+        {
+            // Name the Windows Service
+            ServiceName = "InsulinWindowsService";
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new InsulinWindowsService());
+        }
+
+        // Start the Windows service.
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            // Create a ServiceHost for the CalculatorService type and 
+            // provide the base address.
+            serviceHost = new WebServiceHost(typeof(InsulinServiceLibrary.InsulinHttpService));
+
+            // Open the ServiceHostBase to create listeners and start 
+            // listening for messages.
+            serviceHost.Open();
+            PrintServiceInfo(serviceHost);
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+
+        static void PrintServiceInfo(WebServiceHost host)
+        {
+            Console.WriteLine("{0} is up and running with these endpoints: ", host.Description.Name);
+            foreach (var se in host.Description.Endpoints)
+            {
+                Console.WriteLine(se.Address);
+            }
+        }
+    }
+}
---
+++ InsulinWindowsService/ProjectInstaller.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace InsulinWindowsService
+{
+    // Provide the ProjectInstaller class which allows 
+    // the service to be installed by the Installutil.exe tool
+
+    // run visual studio command prompt as admin.
+    //
+    // 1. installutil bin\Debug\InsulinWindowsService.exe
+    // 2. net start InsulinWindowsService
+    //
+    // To uninstall:
+    // 3. installutil /u bin\Debug\InsulinWindowsService.exe
+    //
+
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "InsulinWindowsService";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
