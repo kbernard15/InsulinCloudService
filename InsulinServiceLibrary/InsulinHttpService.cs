9ecdfb4076d9a3f4492711d691046f1abf88e3b3
keith bernard <kbernard@logikos.com>
2016-12-09 13:28:13 -0500

    removed unused usings

--- InsulinServiceLibrary/InsulinHttpService.cs
+++ InsulinServiceLibrary/InsulinHttpService.cs
@@ -4,11 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
 namespace InsulinServiceLibrary
 {
